ddf8d449fc8c3547c8cb937f32e32ccf11a72765
Andreas Heider <andreas@heider.io>
2018-02-01 00:30:13 +0000

    Comment USB descriptors

--- src/usb/descriptors.rs
+++ src/usb/descriptors.rs
@@ -1,59 +1,54 @@
 pub const DEV_DESC: [u8; 18] = [
-    0x12,
-    0x01,
-    0x00,
-    0x02,
-    0x00,
-    0x00,
-    0x00,
-    0x40,
-    0xff,
-    0xff,
-    0xff,
-    0xff,
-    0x01,
-    0x00,
-    0x01,
-    0x02,
-    0x03,
-    0x01,
+    0x12,        // bLength
+    0x01,        // bDescriptorType (Device)
+    0x00, 0x02,  // bcdUSB 2.00
+    0x00,        // bDeviceClass (Use class information in the Interface Descriptors)
+    0x00,        // bDeviceSubClass
+    0x00,        // bDeviceProtocol
+    0x40,        // bMaxPacketSize0 64
+    0xFF, 0xFF,  // idVendor 0xFFFF
+    0xFF, 0xFF,  // idProduct 0xFFFF
+    0x01, 0x00,  // bcdDevice 0.01
+    0x01,        // iManufacturer (String Index)
+    0x02,        // iProduct (String Index)
+    0x03,        // iSerialNumber (String Index)
+    0x01,        // bNumConfigurations 1
 ];
 
 pub const CONF_DESC: [u8; 34] = [
-    0x09,
-    0x02,
-    0x22,
-    0x00,
-    0x01,
-    0x01,
-    0x04,
-    0x80,
-    0xfa,
-    0x09,
-    0x04,
-    0x00,
-    0x00,
-    0x01,
-    0x03,
-    0x01,
-    0x01,
-    0x05,
-    0x09,
-    0x21,
-    0x11,
-    0x01,
-    0x00,
-    0x01,
-    0x22,
-    0x29,
-    0x00,
-    0x07,
-    0x05,
-    0x81,
-    0x03,
-    0x40,
-    0x00,
-    0x01,
+    0x09,        // bLength
+    0x02,        // bDescriptorType (Configuration)
+    0x22, 0x00,  // wTotalLength
+    0x01,        // bNumInterfaces
+    0x01,        // bConfigurationValue
+    0x04,        // iConfiguration (String Index)
+    0x80,        // bmAttributes
+    0xFA,        // bMaxPower 500mA
+
+    0x09,        // bLength
+    0x04,        // bDescriptorType (Interface)
+    0x00,        // bInterfaceNumber 0
+    0x00,        // bAlternateSetting
+    0x01,        // bNumEndpoints 1
+    0x03,        // bInterfaceClass
+    0x01,        // bInterfaceSubClass
+    0x01,        // bInterfaceProtocol
+    0x05,        // iInterface (String Index)
+
+    0x09,        // bLength
+    0x21,        // bDescriptorType (HID)
+    0x11, 0x01,  // bcdHID 1.11
+    0x00,        // bCountryCode
+    0x01,        // bNumDescriptors
+    0x22,        // bDescriptorType[0] (HID)
+    0x29, 0x00,  // wDescriptorLength[0] 41
+
+    0x07,        // bLength
+    0x05,        // bDescriptorType (Endpoint)
+    0x81,        // bEndpointAddress (IN/D2H)
+    0x03,        // bmAttributes (Interrupt)
+    0x40, 0x00,  // wMaxPacketSize 64
+    0x01,        // bInterval 1 (unit depends on device speed)
 ];
 
 pub const HID_REPORT_DESC: [u8; 41] = [
@@ -80,11 +75,23 @@ pub const HID_REPORT_DESC: [u8; 41] = [
     0xC0,       // End Collection
 ];
 
-pub const DEVICE_QUALIFIER: [u8; 10] = [0x0a, 0x06, 0x00, 0x02, 0x00, 0x00, 0x40, 0x01, 0x00, 0x00];
+pub const DEVICE_QUALIFIER: [u8; 10] = [
+    0x0A,        // bLength
+    0x06,        // bDescriptorType (Device Qualifier)
+    0x00, 0x02,  // bcdUSB 2.00
+    0x00,        // bDeviceClass (Use class information in the Interface Descriptors)
+    0x00,        // bDeviceSubClass
+    0x40,        // bDeviceProtocol 
+    0x01,        // bMaxPacketSize0 1
+    0x00,        // bNumConfigurations 0
+    0x00,        // bReserved
+];
 
-pub const LANG_STR: [u8; 4] = [0x04, 0x03, 0x09, 0x04];
+pub const LANG_STR: [u8; 4] = [
+    0x04, 0x03, //
+    0x09, 0x04, // English - US
+];
 
-// String Descriptor 1, "Rusty Manufacturer"
 pub const MANUFACTURER_STR: [u8; 38] = [
     0x26, 0x03, //
     0x52, 0x00, // R
@@ -107,7 +114,6 @@ pub const MANUFACTURER_STR: [u8; 38] = [
     0x72, 0x00, // r
 ];
 
-// String Descriptor 2, "Rusty Product"
 pub const PRODUCT_STR: [u8; 28] = [
     0x1c, 0x03, //
     0x52, 0x00, // R
@@ -136,44 +142,24 @@ pub const SERIAL_NUMBER_STR: [u8; 14] = [
 ];
 
 pub const CONF_STR: [u8; 40] = [
-    0x28,
-    0x03,
-    0x52,
-    0x00,
-    0x75,
-    0x00,
-    0x73,
-    0x00,
-    0x74,
-    0x00,
-    0x79,
-    0x00,
-    0x20,
-    0x00,
-    0x43,
-    0x00,
-    0x6f,
-    0x00,
-    0x6e,
-    0x00,
-    0x66,
-    0x00,
-    0x69,
-    0x00,
-    0x67,
-    0x00,
-    0x75,
-    0x00,
-    0x72,
-    0x00,
-    0x61,
-    0x00,
-    0x74,
-    0x00,
-    0x69,
-    0x00,
-    0x6f,
-    0x00,
-    0x6e,
-    0x00,
+    0x28, 0x03, //
+    0x52, 0x00, // R
+    0x75, 0x00, // u
+    0x73, 0x00, // s
+    0x74, 0x00, // t
+    0x79, 0x00, // y
+    0x20, 0x00, //
+    0x43, 0x00, // C
+    0x6f, 0x00, // o
+    0x6e, 0x00, // n
+    0x66, 0x00, // f
+    0x69, 0x00, // i
+    0x67, 0x00, // g
+    0x75, 0x00, // u
+    0x72, 0x00, // r
+    0x61, 0x00, // a
+    0x74, 0x00, // t
+    0x69, 0x00, // i
+    0x6f, 0x00, // o
+    0x6e, 0x00, // n
 ];
