1675b8165db9f568cfbd79fbd54b542ede74bc2d
Andreas Heider <andreas@heider.io>
2018-02-05 22:20:11 +0000

    Missing USB constants

---
+++ src/usb/constants.rs
@@ -0,0 +1,45 @@
+#![allow(dead_code)]
+
+use core::mem::transmute;
+
+#[repr(u8)]
+#[non_exhaustive]
+#[derive(Debug,Copy,Clone)]
+pub enum UsbRequest {
+    GetStatus = 0x00,
+    ClearFeature = 0x01,
+    SetFeature = 0x03,
+    SetAddress = 0x05,
+    GetDescriptor = 0x06,
+    SetDescriptor = 0x07,
+    GetConfiguration = 0x08,
+    SetConfiguration = 0x09,
+    GetInterface = 0x0A,
+    SetInterface = 0x0B,
+    SynchFrame = 0x0C,
+}
+
+impl From<u8> for UsbRequest {
+    #[inline]
+    fn from(b: u8) -> Self { unsafe { transmute(b) } }
+}
+
+#[repr(u8)]
+#[non_exhaustive]
+#[derive(Debug,Copy,Clone)]
+pub enum UsbDescriptorType {
+    Device = 1,
+    Configuration = 2,
+    StringDesc = 3,
+    Interface = 4,
+    Endpoint = 5,
+    DeviceQualifier = 6,
+    OtherSpeedConfiguration = 7,
+    Bos = 0x0F,
+    HidReport = 0x22,
+}
+
+impl From<u8> for UsbDescriptorType {
+    #[inline]
+    fn from(b: u8) -> Self { unsafe { transmute(b) } }
+}
