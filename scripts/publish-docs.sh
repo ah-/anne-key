60bc89c67954ecc322cfbc204b7370c976fd02be
Andreas Heider <andreas@heider.io>
2018-03-01 22:39:04 +0000

    Use travis to build docs

---
+++ scripts/publish-docs.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -euxo pipefail
+
+pushd book
+
+echo -e "Initializing Git"
+git init
+git config user.name "Andreas Heider"
+git config user.email "andreas@heider.io"
+
+git remote add upstream "https://$GH_TOKEN@github.com/ah-/anne-key.git"
+git fetch upstream --quiet
+git reset upstream/gh-pages --quiet
+
+touch .
+
+echo -e "Pushing changes to gh-pages"
+git add -A . 
+git commit -m "rebuild pages at ${rev}" --quiet
+git push -q upstream HEAD:gh-pages --quiet
+
+echo -e "Deployed docs to GitHub Pages"
+
+popd
