9b1a92bda00b1ff2c90fb146d7fc29c9726f5aa7
Andreas Heider <andreas@heider.io>
2018-03-14 23:16:54 +0000

    Bluetooth host numbers start at 1

--- src/layout.rs
+++ src/layout.rs
@@ -59,9 +59,9 @@ pub const FN2: Layout = layout![
 
 #[cfg_attr(rustfmt, rustfmt_skip)]
 pub const BT: Layout = layout![
-    LayerOff(LAYER_BT) BtConnectHost(0) BtConnectHost(1) BtConnectHost(2) BtConnectHost(3) __ __ __ __ __ BtToggleCompatibilityMode BtOff BtBroadcast BtOn
-    __ BtSaveHost(0) BtSaveHost(1) BtSaveHost(2) BtSaveHost(3) __ __ __ __ __ __ __ __ __
-    __ BtDeleteHost(0) BtDeleteHost(1) BtDeleteHost(2) BtDeleteHost(3) __ __ __ __ __ __ __ No __
+    LayerOff(LAYER_BT) BtConnectHost(1) BtConnectHost(2) BtConnectHost(3) BtConnectHost(4) __ __ __ __ __ BtToggleCompatibilityMode BtOff BtBroadcast BtOn
+    __ BtSaveHost(1) BtSaveHost(2) BtSaveHost(3) BtSaveHost(4) __ __ __ __ __ __ __ __ __
+    __ BtDeleteHost(1) BtDeleteHost(2) BtDeleteHost(3) BtDeleteHost(4) __ __ __ __ __ __ __ No __
     __ __ __ __ __ LayerOff(LAYER_BT) __ __ __ __ __ __ __ __
     BtHostListQuery __ __ No No __ No No No No __ __ __ __
 ];
