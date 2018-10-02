48ac8dd7b38311f2743e2e57ee4b0c7aa2066e39
Hoàng Đức Hiếu <hdhoang@hdhoang.space>
2018-10-02 12:09:49 +0700

    travis: install osx tools with install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,6 +12,11 @@ main() {
             mkdir gcc
             curl -L https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-linux.tar.bz2?revision=bc2c96c0-14b5-4bb4-9f18-bceb4050fee7?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Linux,7-2018-q2-update | tar --strip-components=1 -C gcc -xj
         fi
+
+        if [ ${TRAVIS_OS_NAME} = 'osx' ]; then
+            brew tap mbartling/gcc-arm-none-eabi
+            brew install gcc-arm-none-eabi
+        fi
 }
 
 main
