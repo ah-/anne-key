cbb9fa85937cdf92186645be8aa5e17f799f4d86
Hoàng Đức Hiếu <hdhoang@hdhoang.space>
2018-10-22 07:53:27 +0700

    travis: add failing windows job
    cargo-bloat doesn't support windows. But it's still failing for no
    reason.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,8 @@ set -eux
 main() {
         which cargo-bloat || (cd /; cargo install cargo-bloat)
         which cargo-objcopy || (cd /; cargo install cargo-binutils)
+        # For Travis CI on Windows
+        which make || choco install make python2
 }
 
 main
