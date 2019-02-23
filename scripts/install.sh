b70650878b68e289590ca7895d06a786fe3f4560
Hoàng Đức Hiếu <code@hdhoang.space>
2019-02-23 08:47:25 +0700

    travis: try to fix windows build

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,10 +3,12 @@
 set -eux
 
 main() {
+    if [[ ${TRAVIS_OS_NAME} != 'windows' ]]; then
         which cargo-bloat || (cd /; cargo install cargo-bloat)
+    else
+        choco install make python2
+    fi
         which cargo-objcopy || (cd /; cargo install cargo-binutils)
-        # For Travis CI on Windows
-        which make || choco install make python2
 }
 
 main
